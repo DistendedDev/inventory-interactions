fa784a54b5f5c5da84fbc0df2fb132738951729e
DistendedDev <diztend@gmail.com>
2022-03-24 16:55:49 +0800

    added configs

--- src/main/java/diztend/quickrepair/Quickrepair.java
+++ src/main/java/diztend/quickrepair/Quickrepair.java
@@ -1,10 +1,17 @@
 package diztend.quickrepair;
 
+import diztend.quickrepair.config.ModConfigs;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Quickrepair implements ModInitializer {
+
+    public static String MOD_ID = "quickrepair";
+    public static Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitialize() {
-
+        ModConfigs.registerConfigs();
     }
 }
--- src/main/java/diztend/quickrepair/mixin/ToolItemMixin.java
+++ src/main/java/diztend/quickrepair/mixin/ToolItemMixin.java
@@ -1,5 +1,6 @@
 package diztend.quickrepair.mixin;
 
+import diztend.quickrepair.config.ModConfigs;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.PlayerEntity;
@@ -7,6 +8,7 @@ import net.minecraft.inventory.StackReference;
 import net.minecraft.item.*;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.screen.slot.Slot;
+import net.minecraft.text.LiteralText;
 import net.minecraft.util.ClickType;
 import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,15 +24,22 @@ public abstract class ToolItemMixin {
     public boolean onClicked(ItemStack stack, ItemStack otherStack, Slot slot, ClickType clickType, PlayerEntity player, StackReference cursorStackReference) {
         if (clickType == ClickType.RIGHT){
             if (stack.isDamaged()) {
-                if (stack.getItem().canRepair(stack, otherStack)) {
-                    stack.setDamage((int) Math.max(stack.getDamage() - Math.floor(stack.getMaxDamage() * 0.25), 0));
+                if (stack.getItem().canRepair(stack, otherStack) && ModConfigs.DO_UNIT_REPAIR > 0) {
+                    stack.setDamage((int) Math.max(stack.getDamage() - Math.floor(stack.getMaxDamage() * ModConfigs.UNIT_REPAIR_RATE), 0));
                     otherStack.decrement(1);
                     return true;
-                } else if (stack.getItem() == otherStack.getItem()) {
-                    stack.setDamage(Math.max(stack.getDamage() - otherStack.getMaxDamage() + otherStack.getDamage(), 0));
-                    otherStack.decrement(1);
-                    return true;
+                } else if (stack.getItem() == otherStack.getItem() && ModConfigs.DO_ITEM_COMBINE > 0) {
+                    if (!stack.hasEnchantments() && !otherStack.hasEnchantments()){
+                        stack.setDamage(Math.max(stack.getDamage() - otherStack.getMaxDamage() + otherStack.getDamage(), 0));
+                        otherStack.decrement(1);
+                        return true;
+                    }
                 }
+            }
+            if (otherStack.getItem() == Items.NAME_TAG && ModConfigs.DO_ITEM_NAMING > 0) {
+                otherStack.decrement(1);
+                stack.setCustomName(otherStack.getName());
+                return true;
             }
         }
         return false;
